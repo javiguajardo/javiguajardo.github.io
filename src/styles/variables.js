9e001ec42ae9074e38faf07ef2bd5118fc76d936
javiguajardo <f.javi.guajardo@gmail.com>
2020-09-21 20:36:19 -0600

    Change color orange

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -1,6 +1,6 @@
 const colorWhite = '#e7e7e7';
 const colorLightGray = '#606060';
-const colorPrimaryOrange = '#e94560';
+const colorPrimaryOrange = '#ec625f';
 const colorDarkBlue = '#1a1a2e';
 
 const sizes = {
