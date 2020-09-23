17413067d629f0d92aed9d4595a9a051b453fbe8
javiguajardo <f.javi.guajardo@gmail.com>
2020-09-22 20:14:18 -0600

    Add SEO tags

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as Main } from './main';
 export { default as ThemeToggle } from './theme-toggle'
 export { default as Footer } from './footer';
 export { default as Layout } from './layout';
+export { default as SEO } from './seo';
